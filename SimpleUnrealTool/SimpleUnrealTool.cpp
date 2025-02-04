cc1e5932572a9122956a38dd7f2e7b3c6605cbf0
Enzo HOURLAY <enzo.hourlay@etu.univ-amu.fr>
2025-02-04 05:39:00 -0500

    feat : build

--- SimpleUnrealTool/SimpleUnrealTool.cpp
+++ SimpleUnrealTool/SimpleUnrealTool.cpp
@@ -10,8 +10,7 @@ namespace fs = std::filesystem;
 
 bool IsUnrealFromSource(const std::string& enginePath) {
     fs::path engineDirectory(enginePath);
-
-    // Remonter de deux niveaux
+    
     engineDirectory = engineDirectory.parent_path().parent_path().parent_path();
     
     fs::path solutionPath = engineDirectory / "UE5.sln";
@@ -46,8 +45,7 @@ void ShowProjectInfo(const std::string& ProjectFilePath)
     {
         std::cout << "- " << plugin["Name"] << std::endl;
     }
-
-    // Vérification si Unreal est compilé from source
+    
     if (IsUnrealFromSource(ProjectFilePath))
     {
         std::cout << "Unreal Engine est from Source" << std::endl;
@@ -58,9 +56,39 @@ void ShowProjectInfo(const std::string& ProjectFilePath)
     }
 }
 
+std::string GetProjectName(const std::string& projectFilePath) {
+    std::ifstream file(projectFilePath);
+    if (!file.is_open()) {
+        std::cerr << "Impossible d'ouvrir le fichier .uproject." << std::endl;
+        return "";
+    }
+
+    nlohmann::json projectJson;
+    file >> projectJson;
+    
+    std::string projectName = projectJson["Modules"][0].value("Name", "");
+    return projectName.empty() ? projectFilePath.substr(projectFilePath.find_last_of("\\") + 1, projectFilePath.find_last_of(".") - projectFilePath.find_last_of("\\") - 1) : projectName;
+}
+
+void BuildProject(const std::string& projectFilePath) {
+    std::string projectName = GetProjectName(projectFilePath);
+    if (projectName.empty()) {
+        std::cerr << "Impossible de récupérer le nom du projet." << std::endl;
+        return;
+    }
+    std::string buildPath = ".\\Engine\\Build\\BatchFiles\\Build.bat";
+    std::string command = buildPath + " " + projectName + " Development Win64 " + projectFilePath + " -waitmutex -progress -NoHotReloadFromIDE";
+    int result = std::system(command.c_str());
+    if (result != 0) {
+        std::cerr << "Échec de la compilation.\n";
+    } else {
+        std::cout << "Compilation réussie.\n";
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        std::cerr << "Usage : SimpleUnrealTool [UPROJECT_PATH] [COMMAND] [OPTION]\n";
+        std::cerr << "Usage : MyTool [UPROJECT_PATH] [COMMAND] [OPTION]\n";
         return 1;
     }
 
@@ -69,9 +97,12 @@ int main(int argc, char* argv[]) {
 
     if (command == "show-infos") {
         ShowProjectInfo(projectPath);
+    } else if (command == "build") {
+        BuildProject(projectPath);
     } else {
         std::cerr << "Commande inconnue.\n";
     }
 
     return 0;
 }
+
